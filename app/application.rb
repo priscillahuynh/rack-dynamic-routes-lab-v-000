5d3ef3c3b7d2d177a8ea4127165fc537bcfef9ef
Priscilla H <priscillamhuynh@gmail.com>
2019-01-05 11:24:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,10 +6,12 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match("/items/").last
-      item = @@items.find {|i| i.name == item_name}
-    else
+    item = @@items.find {|i| i.name == item_name}
+      if item.nil?
       resp.write "Route not found"
       resp.status = 404
+      else
+      resp.write item.price
     end
     resp.finish
   end
