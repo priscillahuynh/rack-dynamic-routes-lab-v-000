c95d24c4dd6524397b8c8de1e598a2c1432dc398
Priscilla H <priscillamhuynh@gmail.com>
2019-01-05 11:21:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,6 @@ class Application
 
     if req.path.match("/items/").last
       item = @@items.find {|i| i.name == item_name}
-      resp.write "You requested item name"
     else
       resp.write "Route not found"
       resp.status = 404
