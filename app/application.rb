f6a29358fea54b567d5ce8371066dd9395af5f93
Priscilla H <priscillamhuynh@gmail.com>
2019-01-05 11:21:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,12 @@
 class Application
   @@items = []
-  
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match("/items/").last
+      item = @@items.find {|i| i.name == item_name}
       resp.write "You requested item name"
     else
       resp.write "Route not found"
